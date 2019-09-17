3d8ba272b51c3981916c261d18aa8be73aeafa58
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-17 13:39:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,5 +16,25 @@ class Song
     @@genres << genre
     @@artists << artist
   end
-  
+
   def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genres.each_with_object(Hash.new(0)){|key, hash| hash[key] += 1}
+  end
+
+  def self.artist_count
+    @@artists.each_with_object(Hash.new(0)){|key, hash| hash[key] += 1}
+  end
+
+end
